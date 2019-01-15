36357eaa57daa43e1dd96973b9faa7ca00b75fc5
Keyon Jie <yang.jie@linux.intel.com>
2019-01-15 15:03:26 +0800

    pipeline: stop pipeline at xrun if no internal xrun recovery
    If no FW internal xrun recovery is chosen, we need to stop pipeline, set
    status to be prepare, and waiting for recovery triggering from host
    side.
    
    Signed-off-by: Keyon Jie <yang.jie@linux.intel.com>

--- src/audio/pipeline.c
+++ src/audio/pipeline.c
@@ -210,9 +210,18 @@ static void pipeline_trigger_sched_comp(struct pipeline *p,
 
 	switch (cmd) {
 	case COMP_TRIGGER_PAUSE:
-	case COMP_TRIGGER_STOP:
 		pipeline_schedule_cancel(p);
 		p->status = COMP_STATE_PAUSED;
+		break;
+#if NO_XRUN_RECOVERY
+	/* stop pipeline at xrun if no internal xrun recovery */
+	case COMP_TRIGGER_XRUN:
+		/* fall through */
+#endif
+	case COMP_TRIGGER_STOP:
+		trace_pipe_with_ids(p, "pipeline_trigger_sched_comp(): STOP");
+		pipeline_schedule_cancel(p);
+		p->status = COMP_STATE_PREPARE;
 		break;
 	case COMP_TRIGGER_RELEASE:
 	case COMP_TRIGGER_START:
@@ -231,7 +240,6 @@ static void pipeline_trigger_sched_comp(struct pipeline *p,
 		break;
 	case COMP_TRIGGER_SUSPEND:
 	case COMP_TRIGGER_RESUME:
-	case COMP_TRIGGER_XRUN:
 	default:
 		break;
 	}
